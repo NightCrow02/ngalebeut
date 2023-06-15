baa899f15607c7791a1e939bf4dafe77012fafd4
mKevinSF <kevinsafaat007@gmail.com>
2023-06-15 12:27:35 +0700

    Aing maung

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'ngalebeutDB';
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname) or die('connection failed');
+
+?>
